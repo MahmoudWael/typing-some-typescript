24dc8e201345f22b49351bca9b6e38afa5cbe37d
mahmoudwael <mahmoud.wael@3ddx.com>
2019-02-14 14:57:36 +0200

    defining a class

---
+++ classes.ts
@@ -0,0 +1,28 @@
+module classes_demo {
+    class Engine {
+        constructor(public horsePower: number, public engineType: string) { }
+    }
+
+    class Car {
+        private _engine: Engine;
+        constructor(enigne: Engine) {
+            this.engine = enigne;
+        }
+        get engine(): Engine {
+            return this._engine
+        }
+        set engine(value: Engine) {
+            if (value == undefined) throw 'Please provide an engine';
+            this._engine = value;
+        }
+        start() {
+            console.log('Car engine: ' + this._engine.engineType + ' started!');
+        }
+    }
+
+    var engine = new Engine(300, 'v8');
+    var car = new Car(engine);
+    var e = new Engine(500, 'BT3');
+    car.engine = e;
+    car.start();
+}
